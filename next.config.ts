65ba0dfd738aa53019aa3a6b6089703007e6333b
alt-safiro <altxafiro@gmail.com>
2024-12-14 00:23:58 +0300

    Add more resolutions for different DPI

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    contentDispositionType: 'inline'
+    contentDispositionType: 'inline',
+    deviceSizes: [480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 3840]
   }
 };
 
