bc4de43507c2903e478711dc2c61d05c0ab764df
alt-safiro <altxafiro@gmail.com>
2024-12-20 01:55:34 +0300

    Fix hardcoded default values

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   images: {
     contentDispositionType: 'inline',
-    deviceSizes: [480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 3840]
+    deviceSizes: [...imageConfigDefault.deviceSizes, 480, 960, 1440]
   }
 };
 
