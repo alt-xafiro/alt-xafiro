e7bfd37ae11a8f7119cfc560822aef2732cdc973
Safiro <altxafiro@gmail.com>
2025-01-06 21:49:29 +0300

    Fix list of ESlint dirs

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,25 @@ import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  eslint: {
+    dirs: [
+      'app',
+      'components',
+      'consts',
+      'hooks',
+      'i18n',
+      'lib',
+      'model',
+      'pages',
+      'public',
+      'services',
+      'src',
+      'store',
+      'types',
+      'ui',
+      'utils'
+    ]
+  },
   images: {
     contentDispositionType: 'inline',
     deviceSizes: [...imageConfigDefault.deviceSizes, 480, 960, 1440]
