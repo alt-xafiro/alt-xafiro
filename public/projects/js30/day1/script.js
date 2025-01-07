009c95f886f4de32ca83486faf8b20b9a228b5b8
Safiro <altxafiro@gmail.com>
2025-01-08 02:04:26 +0300

    Add Day 1 of #JavaScript30 project

---
+++ public/projects/js30/day1/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const keyCodes = {
+  65: 'KeyA',
+  83: 'KeyS',
+  68: 'KeyD',
+  70: 'KeyF',
+  71: 'KeyG',
+  72: 'KeyH',
+  74: 'KeyJ',
+  75: 'KeyK',
+  76: 'KeyL'
+};
+
+class Drum {
+  constructor(key) {
+    this.keyNode = document.querySelector(`.key[data-key="${key}"]`);
+    this.audioNode = document.querySelector(`audio[data-key="${key}"]`);
+
+    this._animationClass = 'playing';
+    this._animationDuration = Math.round(
+      parseFloat(
+        window
+          .getComputedStyle(this.keyNode)
+          .getPropertyValue('transition-duration')
+      ) * 1000
+    );
+    this._animationTimer = null;
+  }
+
+  play() {
+    this.audioNode.currentTime = 0;
+    this.audioNode.play();
+  }
+
+  animate() {
+    this.keyNode.classList.add(this._animationClass);
+
+    if (this._animationTimer) {
+      clearTimeout(this._animationTimer);
+    }
+
+    this._animationTimer = setTimeout(() => {
+      this.keyNode.classList.remove(this._animationClass);
+    }, this._animationDuration);
+  }
+}
+const generateDrums = () => {
+  const result = {};
+
+  Object.keys(keyCodes).forEach(
+    (drumKey) => (result[keyCodes[drumKey]] = new Drum(drumKey))
+  );
+
+  return result;
+};
+
+const Drums = generateDrums();
+
+const onKeydown = (evt) => {
+  const keyCode = evt.code;
+
+  if (Object.values(keyCodes).includes(keyCode)) {
+    evt.preventDefault();
+
+    Drums[keyCode].play();
+    Drums[keyCode].animate();
+  }
+};
+
+window.addEventListener('keydown', onKeydown);
