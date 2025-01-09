2b5f76d2d13940d3b53ca4728165e082f532d710
Safiro <altxafiro@gmail.com>
2025-01-09 07:35:00 +0300

    Fix tooltip overflow

--- components/common-tooltip/common-tooltip.tsx
+++ components/common-tooltip/common-tooltip.tsx
@@ -3,7 +3,7 @@ import { Tooltip } from 'react-tooltip';
 export default function CommonTooltip() {
   return (
     <Tooltip
-      className="!z-[9000] !rounded-[16px] !bg-space-700/95 !text-base !transition-none !duration-0 h-md:!text-sm sm:!text-sm"
+      className="!z-[9000] !max-w-[600px] !rounded-[16px] !bg-space-700/95 !text-base !transition-none !duration-0 h-md:!text-sm sm:!text-sm"
       anchorSelect=".tooltip"
       opacity={1}
       place="top"
