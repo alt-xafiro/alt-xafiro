3cc2ad1e26b97043ffa8d6bebf5bfb1ac9418a4b
Safiro <altxafiro@gmail.com>
2025-01-09 07:34:25 +0300

    Add #JavaScript30 description

--- components/js30-challenge/js30-challenge.tsx
+++ components/js30-challenge/js30-challenge.tsx
@@ -22,12 +22,16 @@ export default function Js30Challenge({ className, data }: Js30ChallengeProps) {
       className={clsx(
         className,
         sourceCodePro.className,
-        'flex flex-col items-center justify-start gap-16 pb-32 lg:gap-10 lg:pb-20 sm:gap-6 sm:pb-16'
+        'flex flex-col items-center justify-start gap-12 pb-32 lg:gap-10 lg:pb-20 sm:gap-6 sm:pb-16'
       )}
     >
       <h1 className="text-center text-6xl font-bold lg:text-5xl sm:text-4xl">
         <ExternalLink href={CHALLENGE_LINK}>{t('Pages.js30')}</ExternalLink>
       </h1>
+
+      <p className="mb-4 max-w-3xl text-justify text-xl lg:text-lg sm:text-base">
+        {t('Js30.description')}
+      </p>
 
       <ul className="flex max-w-2xl flex-col items-start justify-start gap-8 text-left lg:gap-6 sm:gap-4">
         {data.projectLocales.map((projectLocale, i) => {
