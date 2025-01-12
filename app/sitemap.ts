aa15ebfd17f0848dff58dab08eef6a87600fb2d9
Safiro <altxafiro@gmail.com>
2025-01-12 06:32:45 +0300

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://xafiro.site',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1
+    },
+    {
+      url: 'https://xafiro.site/projects',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1
+    },
+    {
+      url: 'https://xafiro.site/projects/js30',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.4
+    },
+    {
+      url: 'https://xafiro.site/socials',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
+    }
+  ];
+}
