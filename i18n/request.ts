0836316c1df1323d091ea20b34c589b0b638d4ff
Safiro <altxafiro@gmail.com>
2025-01-13 23:29:26 +0300

    Add fallback language

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,12 +1,21 @@
-import { getRequestConfig } from 'next-intl/server';
+import deepmerge from 'deepmerge';
+import { RequestConfig, getRequestConfig } from 'next-intl/server';
 
+import { LOCALES } from '@/consts';
 import { getUserLocale } from '@/services/locale';
 
 export default getRequestConfig(async () => {
   const locale = await getUserLocale();
 
+  const defaultMessages = (await import(`@/messages/${LOCALES[0]}.json`))
+    .default;
+  const userMessages = (await import(`@/messages/${locale}.json`)).default;
+
   return {
     locale,
-    messages: (await import(`@/messages/${locale}.json`)).default
-  };
+    messages:
+      locale === LOCALES[0]
+        ? defaultMessages
+        : deepmerge(defaultMessages, userMessages)
+  } as RequestConfig;
 });
